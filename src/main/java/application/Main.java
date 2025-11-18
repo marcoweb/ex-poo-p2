e1541b2372fe2271735ef4f463356cbd19d9a68a
ETEC <164@etec.sp.gov.br>
2025-11-18 01:09:59 +0000

    Aula 17/11/2025

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,7 +2,20 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        PagamentoCartaoCredito pag1 = new PagamentoCartaoCredito(
+            200, "Exemplo de Pagamento", "123454323456",
+            "Fulano de Tal", "11/26", "345", 2);
+        
+        pag1.exibirDetalhesPagamento();
+        
+        Priorizavel pri = pag1;
+        Pagamento pag2 = pag1;
+
+        pri.definirPrioridade(4);
+
+        System.out.println(pag1.obterNivelPrioridade());
+
+        pag1.processar();
     }
 }
 
